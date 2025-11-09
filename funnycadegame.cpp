648758ec430a4294505fd28c7ac37f7fdc320df5
ThatCreeper <creeperhostanimations@gmail.com>
2025-11-09 16:46:17 -0600

    implement reusable_inplace_vector for connections

--- funnycadegame.cpp
+++ funnycadegame.cpp
@@ -13,6 +13,36 @@
 #undef DrawText
 #define strncpy strncpy_s
 
+template <class T>
+struct optional_ref {
+	T *val;
+	optional_ref() : val(nullptr) {}
+	optional_ref(T &v) : val(&v) {}
+	optional_ref(optional_ref<T> &v) : val(v.val) {}
+
+	T &value() {
+		assert(val);
+		return *val;
+	}
+	T *operator ->() {
+		return val;
+	}
+	T &operator *() {
+		return value();
+	}
+	operator bool() const {
+		return val;
+	}
+
+	void reset() {
+		val = nullptr;
+	}
+	T &emplace(T &v) {
+		val = &v;
+		return v;
+	}
+};
+
 template <class T, size_t N>
 struct inplace_vector {
 	union {
@@ -32,6 +62,12 @@ struct inplace_vector {
 	inplace_vector<T, N> &operator =(const inplace_vector<T, N> &other) = delete;
 	inplace_vector<T, N> &operator =(inplace_vector<T, N> &&other) = delete;
 
+	const T *begin() const {
+		return values;
+	}
+	const T *end() const {
+		return values + count;
+	}
 	T *begin() {
 		return values;
 	}
@@ -39,28 +75,129 @@ struct inplace_vector {
 		return values + count;
 	}
 
-	size_t size() {
+	size_t size() const {
 		return count;
 	}
-	size_t capacity() {
+	size_t capacity() const {
 		return N;
 	}
 
 	template<class... Args>
-	std::optional<T &> try_emplace_back(Args&&... args) {
+	optional_ref<T> try_emplace_back(Args&&... args) {
 		if (full()) return {};
-		T *out = values[count++];
+		T *out = &values[count++];
 		new(out) T(std::forward(args)...);
 		return *out;
 	}
 
-	bool full() {
+	bool full() const {
 		return count >= N;
 	}
 
-	std::optional<T &>try_at(int pos) {
+	optional_ref<const T> try_at(int pos) const {
 		if (pos >= count) return {};
 		return values[pos];
+	}
+	optional_ref<T> try_at(int pos) {
+		if (pos >= count) return {};
+		return values[pos];
+	}
+};
+
+template <class T, size_t N>
+struct reusable_inplace_vector {
+	using parent = inplace_vector<std::optional<T>, N>;
+	parent store;
+	int reusables = 0;
+
+	bool full() {
+		return store.count >= N && reusables <= 0;
+	}
+
+	struct iterator {
+		std::optional<T> *p;
+		reusable_inplace_vector<T, N> &v;
+		T *operator ->() {
+			return &**p;
+		}
+		T &operator *() {
+			return **p;
+		}
+		iterator &operator ++() {
+			std::optional<T> *end = v.end().p;
+			while (p != end) {
+				p++;
+				if (*p) return *this;
+			}
+			return *this;
+		}
+		bool operator==(const iterator &other) {
+			return p == other.p;
+		}
+		bool operator!=(const iterator &other) {
+			return p != other.p;
+		}
+	};
+
+	iterator begin() {
+		iterator it{ .p = store.begin(), .v = *this };
+		iterator ie = end();
+		while (it != ie) {
+			if (*(it.p)) return it;
+			++it;
+		}
+		return ie;
+	}
+
+	iterator end() {
+		return { .p = store.end(), .v = *this };
+	}
+
+	template<class... Args>
+	optional_ref<T> try_emplace_replace(Args&&... args) {
+		if (full()) return {};
+		if (reusables > 0) {
+			for (std::optional<T> &member : store) {
+				if (member) continue;
+				member.emplace(std::forward(args)...);
+				reusables--;
+				return *member;
+			}
+		}
+		optional_ref<std::optional<T>> out = store.try_emplace_back();
+		(*out).emplace(std::forward(args)...);
+		return **out;
+	}
+
+	std::optional<size_t> index(T &member) {
+		// this is kinda stupid but i don't want to do
+		// anything smarter so take that
+		for (int i = 0; i < store.size(); i++) {
+			std::optional<T> &val = store.values[i];
+			if (!val) continue;
+			if (&*val == &member) {
+				return i;
+			}
+		}
+		return {};
+	}
+
+	optional_ref<T> try_at(int pos) {
+		optional_ref<std::optional<T>> res = store.try_at(pos);
+		if (!res) return {};
+		return **res;
+	}
+
+	void remove_safe_iter(T &value) {
+		// this is also kinda stupid but i don't want to do
+		// anything smarter so take that
+		for (std::optional<T> &member : store) {
+			if (&*member == &value) {
+				member.reset();
+				reusables++;
+				return;
+			}
+		}
 	}
 };
 
@@ -156,7 +293,7 @@ struct Users {
 		user.idx = idx;
 		return idx;
 	}
-	std::optional<User &> Get(int id) {
+	optional_ref<User> Get(int id) {
 		return users.try_at(id);
 	}
 	size_t size() {
@@ -167,7 +304,6 @@ struct Users {
 struct Connection {
 	int id = 0;
 	SOCKET socket;
-	bool reusable = false;
 };
 
 void clientAcceptPacket(ClientboundPacket &packet);
@@ -176,9 +312,7 @@ void serverAcceptPacket(ServerboundPacket &packet, Connection &connection);
 Connection loopbackConnection;
 bool isServer = false;
 SOCKET serverSocket;
-Connection clientSockets[5];
-int clientSocketc = 0;
-int clientSocketr = 0;
+reusable_inplace_vector<Connection, 5> clientSockets;
 
 void serverOpen() {
 	WSADATA wsaData;
@@ -222,44 +356,27 @@ void clientOpen(const char *host) {
 
 void netRecievePackets() {
 	if (isServer) {
-		if (clientSocketc < 5 || clientSocketr > 0) {
+		if (!clientSockets.full()) {
 			SOCKET socket = accept(serverSocket, nullptr, nullptr);
 			if (socket != INVALID_SOCKET) {
 				unsigned long nonblock = 1;
 				ioctlsocket(socket, FIONBIO, &nonblock);
-				Connection *connection = nullptr;
-				if (clientSocketr > 0) {
-					for (int i = 0; i < clientSocketc; i++) {
-						if (clientSockets[i].reusable) {
-							connection = &clientSockets[i];
-							clientSocketr--;
-							break;
-						}
-					}
-				}
-				else {
-					connection = &clientSockets[clientSocketc++];
-				}
-				assert(connection != nullptr);
-				connection->id = -1;
-				connection->socket = socket;
-				connection->reusable = false;
+				Connection &connection = *clientSockets.try_emplace_replace();
+				connection.id = -1;
+				connection.socket = socket;
 			}
 		}
 
 		ServerboundPacket packet;
-		for (int i = 0; i < clientSocketc; i++) {
-			Connection &connection = clientSockets[i];
-			if (connection.reusable) continue;
+		for (Connection &connection : clientSockets) {
 			while (true) {
 				if (recv(connection.socket, reinterpret_cast<char *>(&packet), sizeof(packet), 0) == SOCKET_ERROR) break;
 				serverAcceptPacket(packet, connection);
 			}
 			if (int e = WSAGetLastError(); e != WSAEWOULDBLOCK) {
-				connection.reusable = true;
-				clientSocketr++;
 				auto user = users.Get(connection.id);
-				MessageBoxA(nullptr, TextFormat("%d: %.*s Disconnect", i, 20, user ? user->name : "(???)"), TextFormat("Conn error: %d", e), MB_OK);
+				MessageBoxA(nullptr, TextFormat("%ll: %.*s Disconnect", clientSockets.index(connection), 20, user ? user->name : "(???)"), TextFormat("Conn error: %d", e), MB_OK);
+				clientSockets.remove_safe_iter(connection); // This is fine(-ish) because it doesn't rearrange anything
 			}
 		}
 	}
@@ -287,8 +404,8 @@ void serverSendPacket(ClientboundPacket &packet, Connection &connection) {
 void serverSendPacketAll(ClientboundPacket &packet) {
 	assert(isServer);
 	clientAcceptPacket(packet);
-	for (int i = 0; i < clientSocketc; i++) {
-		serverSendPacket(packet, clientSockets[i]);
+	for (Connection &connection : clientSockets) {
+		serverSendPacket(packet, connection);
 	}
 }
 
@@ -301,6 +418,7 @@ void clientSendPacket(ServerboundPacket &packet) {
 
 #define PCK(kind) \
 	case ServerboundPacket::Kind::kind: \
+		void serverAcceptPacket##kind(ServerboundPacket &packet, Connection &connection); \
 		serverAcceptPacket##kind(packet, connection); \
 		break
 void serverAcceptPacket(ServerboundPacket &packet, Connection &connection) {
@@ -370,6 +488,7 @@ int playerScores[6] = { 0 };
 
 #define PCK(kind) \
 	case ClientboundPacket::Kind::kind: \
+		void clientAcceptPacket##kind(ClientboundPacket &packet); \
 		clientAcceptPacket##kind(packet); \
 		break
 void clientAcceptPacket(ClientboundPacket &packet) {
