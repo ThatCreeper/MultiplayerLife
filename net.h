1ef7af79219d8c3784ae7600501273b0bda065f4
ThatCreeper <creeperhostanimations@gmail.com>
2025-11-14 19:20:02 -0600

    Get rid of silly thing I added by accident

--- net.h
+++ net.h
@@ -1,14 +1,3 @@
 #pragma once
 
 void netRecievePackets();
-
-struct TCPHeader {
-	short sourcePort;
-	short destinationPort;
-	int sequenceNumber;
-	int acknowledgementNumber;
-	short lengthFlags;
-	short winSize;
-	short checkSum;
-	short urgentPointer;
-};
